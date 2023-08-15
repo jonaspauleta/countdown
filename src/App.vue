a89d5777ff7465a0f2fa0fb7d574c0037c0041cd
João Paulo Santos <jonaspauleta@jonaspaletasMBP.lan>
2023-08-15 21:14:28 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Countdown from "./components/Countdown.vue";
+</script>
+
+<template>
+  <Countdown />
+</template>
---
+++ src/components/Countdown.vue
@@ -0,0 +1,19 @@
+<script setup>
+import VueCountdown from '@chenfengyuan/vue-countdown'
+
+const targetDate = new Date('2023-08-19 10:00:00')
+const time = targetDate - (new Date())
+</script>
+
+<template>
+  <vue-countdown :time="time" v-slot="{ days, hours, minutes, seconds }">
+    Time remaining to see my love ❤️：{{ days }} days, {{ hours }} hours, {{ minutes }} minutes, {{ seconds }} seconds.
+  </vue-countdown>
+</template>
+
+<style scoped>
+span {
+  font-weight: bold;
+  font-size: x-large;
+}
+</style>
